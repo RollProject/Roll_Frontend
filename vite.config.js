16a1d09cc864c9673d2b85d87ef47f7e9295f6e8
min3eo <110177217+min3eo@users.noreply.github.com>
2025-10-02 12:45:46 +0900

    fix: tailwind 설정에서 Inter 폰트 적용 오류 해결

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
 import svgr from "vite-plugin-svgr";
-import { visualizer } from "rollup-plugin-visualizer";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
