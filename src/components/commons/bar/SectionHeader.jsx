3ff0fed33dbb9053b7a27bb85bea2c0c5be97676
min3eo <110177217+min3eo@users.noreply.github.com>
2025-10-17 01:09:35 +0900

    feat: 로고 수정과 폰트 변경

--- src/components/commons/bar/SectionHeader.jsx
+++ src/components/commons/bar/SectionHeader.jsx
@@ -4,7 +4,7 @@ function SectionHeader({ title, children }) {
   return (
     <div className="flex items-center justify-between w-full">
       {/* 왼쪽: 타이틀 */}
-      <h2 className="text-lg font-semibold text-gray-900">{title}</h2>
+      <h2 className="text-lg  text-gray-900">{title}</h2>
 
       {/* 오른쪽: children (MoreButton, Chip 등) */}
       <div className="flex items-center gap-2">{children}</div>
