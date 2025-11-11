3dce60b588d3e7857e42178ddac9f3cf069d54d8
hs-2191214-parkjunseo <02justine@naver.com>
2025-11-12 01:59:26 +0900

    fix : 보드 배경 색상 컬러 전체적으로 연하게 변경

--- src/mock/colors.js
+++ src/mock/colors.js
@@ -1,9 +1,10 @@
 export const DUMMY_COLORS = [
-  "bg-blue-200",
-  "bg-purple-200",
-  "bg-pink-200",
-  "bg-green-200",
-  "bg-lime-200",
-  "bg-orange-200",
   "bg-white",
+  "bg-sky-200",
+  "bg-violet-200",
+  "bg-rose-200",
+  "bg-emerald-200",
+  "bg-yellow-200",
+  "bg-stone-200",
+  "bg-red-200",
 ];
